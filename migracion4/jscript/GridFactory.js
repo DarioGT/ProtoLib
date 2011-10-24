ce23ef49884b45ea59c5a04a91fcd8a9eb32a430
Dario Gomez <dariogomezt@gmail.com>
2011-10-24 10:07:52 -0400

    REtomo 3.4 despues de intentar la migracion a 4.0,
    Los metodos para la construccion dinamica de coumnas cambiaron completamente, ademas del manejo de clases en general ( esto se hizo )  el manejo de clases en 4 es muy supereior al de 3.4,  y permite la carga dinamica de los scripts,  el mismo se encarga de hacer los callback

---
+++ migracion4/jscript/GridFactory.js
@@ -0,0 +1,88 @@
+       
+function GridConfigFactory(protoEntityName) {
+           
+            var protoGrid = new ProtoAutoGrid({
+                autoWidth:true
+                ,border:false
+                ,pagesize:5
+                
+                ,tbar:[
+                    new Ext.Button({
+                        text:'add new user'
+                        ,iconCls:'icon-user_add'
+                        ,scope:this
+                        ,handler:function() {
+                            
+                            UserEditor(arguments[0].findParentByType('AutoGrid'), '');
+                        }
+                    })
+                ]
+                ,forceFit:false
+                ,stripRows:true
+                ,showBbar:true
+                ,loadMask:true
+//              ,sm:new Ext.grid.RowSelectionModel({})
+                ,store: new Ext.data.Store({
+                    autoLoad:true
+                    ,baseParams:{}
+                    ,remoteSort:false
+                    ,sortInfo: {
+                                field: 'id',
+                                direction: 'DESC'
+                            }
+                    ,proxy:new Ext.data.HttpProxy({
+                        url:'protoExtjsGridDefinition/?' + protoEntityName
+                        ,method:'POST'
+                    })
+                    ,reader: new Ext.data.Reader({
+                        root:'rows'
+                      ,id:'id'
+                        ,
+                    })
+
+                })
+            });
+            
+            return protoGrid;
+        
+        }
+
+
+
+function UserEditor(sender, pk) {
+          var editor = new Ext.ux.DjangoForm({
+                border:false
+                ,intro:'this is a generated form'
+                ,showButtons:true
+                ,showSuccessMessage:false
+                ,url:'protoExtjsFormDefinition' 
+                ,baseParamsLoad:{pk:pk}
+                ,width:400
+                ,height:200
+                ,scope:this
+                 ,callback:function(form) {
+                    //console.log(form, this, editor);
+                    form.doLayout();
+                    form.findParentByType('window').center();
+                 }
+           });
+
+           var winEditor = new Ext.Window({
+            items:editor
+            ,title:'generated proto Form'
+           })
+           
+             editor.on('submitSuccess', function() {
+                //console.log('success', this, arguments);
+                        if (sender) sender.store.reload();
+                        if (winEditor && winEditor.isVisible()) {
+                            winEditor.close();
+                            winEditor.destroy();
+                        }
+
+                    }, sender || this);
+           
+           winEditor.show();
+        }
+        
+
---
+++ migracion4/jscript/ProtoAutoGrid.js
@@ -0,0 +1,42 @@
+    
+Ext.define( 'ProtoAutoGrid' =  Ext.extend(ProtoAutoGridPanel, {
+     showBbar:false
+    ,stripeRows:true
+    ,deferredRender :true
+    ,autoSave:false
+    ,remoteSort:true
+    ,sortInfo:{}
+    // ,sm:new Ext.grid.RowSelectionModel({})
+
+    // ,reader: new Ext.data.JsonReader({
+            // root:'rows'
+            // ,id:'id'
+     // })
+    ,initComponent:function() {
+        this.pagesize = this.pagesize || 10;
+        
+        if (this.showBbar) this.bbar = new Ext.PagingToolbar({
+                pageSize: this.pagesize,
+                store:  this.store,
+                displayInfo: true,
+                displayMsg: '{0} à {1} sur {2}',
+                emptyMsg: "Aucun élément à afficher"
+        });
+        
+        var config = {  
+            store:  this.store
+            ,stripeRows: true
+            ,loadMask: true
+            ,autoSave: this.autoSave
+        };
+        Ext.apply(this.initialConfig, config);
+        ProtoAutoGrid.superclass.initComponent.apply(this, arguments);
+
+    } 
+
+ 
+}); 
+
+
+            
+Ext.reg('AutoGrid', 'ProtoAutoGrid'); 
